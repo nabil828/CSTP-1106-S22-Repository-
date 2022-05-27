f57e9f59331edcab8bd4fc0ce6d9ca503487f69e
Nabil M. Al-Rousan <rousani@gmail.com>
2022-05-27 11:12:13 -0700

    JS Intro

---
+++ w4-demo/code.js
@@ -0,0 +1,23 @@
+
+x = 6;
+y = -5 ;
+z = 2 * y - x / 2 + 50
+
+// alert(z)
+// 6 < -8 FALSE
+// 5/2 > 4 TRUE
+// 0 <= 0/2 TRUE
+// 5 >= 4/2 TRUE 
+// x == 12/2 TRUE
+// y != -5 FALSE
+
+if(y > 0){
+    console.log("it is positive")
+}
+else{
+    if(y == 0)
+        console.log("it is zero")
+    else
+        console.log("it is negative")
+}
+    
