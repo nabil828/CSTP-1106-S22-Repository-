6a1ca78619e0beb6441003f76c0cbe464fc21762
Nabil M. Al-Rousan <rousani@gmail.com>
2022-06-03 10:22:42 -0700

    jQuery example

--- w5-demo/code.js
+++ w5-demo/code.js
@@ -1,6 +1,9 @@
 console.log("is working");
 
-function solve_qaud(a, b, c){
+function solve_qaud(){
+    a = $("#a_value").val();
+    b = $("#b_value").val();
+    c = $("#c_value").val();
     if (a == 0) {
         console.log("Is not quadratic");
         if (b == 0) {
@@ -23,11 +26,11 @@ function solve_qaud(a, b, c){
     }
 }
 
-a = 3
-b = 4
-c = 1
+// a = 3
+// b = 4
+// c = 1
 
-solve_qaud(a, b, c)
+// solve_qaud(a, b, c)
 // solve_qaud(2, 2, 1)
 // solve_qaud(2, 2, 1)
 // solve_qaud(2, 2, 1)
@@ -36,6 +39,9 @@ solve_qaud(a, b, c)
 
 function setup (){
     console.log("jQuery works!");
+    $("#calc_the_route_of_quad").click(solve_qaud)
+    
+    // solve_qaud(?, ? , ?)
 }
 
 $(document).ready(setup)
