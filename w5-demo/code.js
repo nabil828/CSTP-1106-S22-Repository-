117106cac1415908d073f625642dcd22ac295de5
Nabil M. Al-Rousan <rousani@gmail.com>
2022-06-03 10:09:22 -0700

    functions!

--- w5-demo/code.js
+++ w5-demo/code.js
@@ -1,26 +1,39 @@
 console.log("is working");
 
-a = 3
-b = 2
-c = 1
-
-if (a == 0) {
-    console.log("Is not quadratic");
-    if (b == 0) {
-        console.log("bye! cuz a=0 and b=0!");
+function solve_qaud(a, b, c){
+    if (a == 0) {
+        console.log("Is not quadratic");
+        if (b == 0) {
+            console.log("bye! cuz a=0 and b=0!");
+        } else {
+            x1 = -1 * c / b
+            console.log(x1);
+        }
     } else {
-        x1 = -1 * c / b
-        console.log(x1);
-    }
-} else {
-    d = b ** 2 - 4 * a * c;
-
-    if (d < 0)
-        console.log("No Real Answer!");
-    else{
-        x1 = (-b + Math.sqrt(d)) / (2 * a)
-        x2 = (-b - Math.sqrt(d)) / (2 * a)
-        console.log(x1);
-        console.log(x2);
+        d = b ** 2 - 4 * a * c;
+    
+        if (d < 0)
+            console.log("No Real Answer!");
+        else{
+            x1 = (-b + Math.sqrt(d)) / (2 * a)
+            x2 = (-b - Math.sqrt(d)) / (2 * a)
+            console.log("X1 : " + x1);
+            console.log("X2 : " + x2);
+        }
     }
 }
+
+a = 3
+b = 4
+c = 1
+
+solve_qaud(a, b, c)
+// solve_qaud(2, 2, 1)
+// solve_qaud(2, 2, 1)
+// solve_qaud(2, 2, 1)
+// solve_qaud(2, 2, 1)
+
+
+
+
+
