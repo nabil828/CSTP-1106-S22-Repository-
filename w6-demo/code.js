d10c2a5f20fe883f926c0bac5562c1393836b87c
Nabil M. Al-Rousan <rousani@gmail.com>
2022-06-10 11:10:36 -0700

    jquery functions

---
+++ w6-demo/code.js
@@ -0,0 +1,33 @@
+function action (){
+    //     val()
+    // html()
+    // before()
+    // after()
+    // append()
+    // prepend()
+    // hide()
+    // show()
+    // remove()
+    // text ()
+    
+    // jQuery("#p1").html("another html content")
+    // console.log( jQuery("#p1").html() )
+
+    // jQuery("#p1").after("another html content")
+    // jQuery("#p1").before("another html content")
+
+    // jQuery("#p1").append("another html content")
+    // jQuery("#p1").prepend("another html content")
+    // jQuery("#p1").hide()
+    // jQuery("#p1").show()
+    // jQuery("#p1").remove()
+    console.log(jQuery("#p1").html())
+
+
+}
+function setup (){
+    jQuery("button").click(action)
+}
+
+
+jQuery(document).ready(setup)
