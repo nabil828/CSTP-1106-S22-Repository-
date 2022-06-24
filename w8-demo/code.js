94c001e459daf0fcb4abf8d4084a54061335ea32
Nabil M. Al-Rousan <rousani@gmail.com>
2022-06-24 11:24:47 -0700

    done

--- w8-demo/code.js
+++ w8-demo/code.js
@@ -3,8 +3,34 @@ function f1(){
     tmp2 = jQuery("#op2").val()
     tmp1 = Number(tmp1)
     tmp2 = Number(tmp2)
-    jQuery("#result").html(tmp1 + tmp2)
+    jQuery("#result").html("Result: " + tmp1 + " + " + tmp2 + " = " + (tmp1 + tmp2))
+    $("#history").append("<span class='add_class'>" +  tmp1 + " + " + tmp2 + " = " + (tmp1 + tmp2) + "<span>" + "<br>" )
 }
+function f2(){
+    tmp1 = jQuery("#op1").val()
+    tmp2 = jQuery("#op2").val()
+    tmp1 = Number(tmp1)
+    tmp2 = Number(tmp2)
+    jQuery("#result").html("Result: " + tmp1 + " + " + tmp2 + " = " + (tmp1 - tmp2))
+    $("#history").append("<span class='sub_class'>" + tmp1 + " + " + tmp2 + " = " + (tmp1 - tmp2) + "<span>" + "<br>" )
+}
+function f3(){
+    tmp1 = jQuery("#op1").val()
+    tmp2 = jQuery("#op2").val()
+    tmp1 = Number(tmp1)
+    tmp2 = Number(tmp2)
+    jQuery("#result").html("Result: " + tmp1 + " + " + tmp2 + " = " + (tmp1 * tmp2))
+    $("#history").append("<span class='mul_class'>" + tmp1 + " + " + tmp2 + " = " + (tmp1 * tmp2) + "<span>" + "<br>" )
+}
+function f4(){
+    tmp1 = jQuery("#op1").val()
+    tmp2 = jQuery("#op2").val()
+    tmp1 = Number(tmp1)
+    tmp2 = Number(tmp2)
+    jQuery("#result").html("Result: " + tmp1 + " + " + tmp2 + " = " + (tmp1 / tmp2))
+    $("#history").append("<span class='div_class'>" + tmp1 + " + " + tmp2 + " = " + (tmp1 / tmp2) + "<span>" + "<br>" )
+}
+
 
 function setup(){
     console.log("setup() got called!");
