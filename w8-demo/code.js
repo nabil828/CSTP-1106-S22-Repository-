91ff590c089afcd22a414148ee40f58653aa5b8e
Nabil M. Al-Rousan <rousani@gmail.com>
2022-06-24 11:30:49 -0700

    Update code.js

--- w8-demo/code.js
+++ w8-demo/code.js
@@ -1,38 +1,47 @@
-function f1(){
+function f1() {
     tmp1 = jQuery("#op1").val()
     tmp2 = jQuery("#op2").val()
     tmp1 = Number(tmp1)
     tmp2 = Number(tmp2)
     jQuery("#result").html("Result: " + tmp1 + " + " + tmp2 + " = " + (tmp1 + tmp2))
-    $("#history").append("<span class='add_class'>" +  tmp1 + " + " + tmp2 + " = " + (tmp1 + tmp2) + "<span>" + "<br>" )
+    $("#history").append(
+        "<span class='add_class'>" +
+        tmp1 +
+        " + " +
+        tmp2 +
+        " = " +
+        (tmp1 + tmp2) +
+        "<span>" +
+        "<br>"
+        )
 }
-function f2(){
+function f2() {
     tmp1 = jQuery("#op1").val()
     tmp2 = jQuery("#op2").val()
     tmp1 = Number(tmp1)
     tmp2 = Number(tmp2)
     jQuery("#result").html("Result: " + tmp1 + " + " + tmp2 + " = " + (tmp1 - tmp2))
-    $("#history").append("<span class='sub_class'>" + tmp1 + " + " + tmp2 + " = " + (tmp1 - tmp2) + "<span>" + "<br>" )
+    $("#history").append(`<span class='sub_class'>  ${tmp1} - ${tmp2}  =  ${(tmp1 - tmp2)}  <span> <br>`)
 }
-function f3(){
+function f3() {
     tmp1 = jQuery("#op1").val()
     tmp2 = jQuery("#op2").val()
     tmp1 = Number(tmp1)
     tmp2 = Number(tmp2)
     jQuery("#result").html("Result: " + tmp1 + " + " + tmp2 + " = " + (tmp1 * tmp2))
-    $("#history").append("<span class='mul_class'>" + tmp1 + " + " + tmp2 + " = " + (tmp1 * tmp2) + "<span>" + "<br>" )
+    $("#history").append("<span class='mul_class'>" + tmp1 + " + " + tmp2 + " = " + (tmp1 * tmp2) + "<span>" + "<br>")
 }
-function f4(){
+function f4() {
     tmp1 = jQuery("#op1").val()
     tmp2 = jQuery("#op2").val()
     tmp1 = Number(tmp1)
     tmp2 = Number(tmp2)
     jQuery("#result").html("Result: " + tmp1 + " + " + tmp2 + " = " + (tmp1 / tmp2))
-    $("#history").append("<span class='div_class'>" + tmp1 + " + " + tmp2 + " = " + (tmp1 / tmp2) + "<span>" + "<br>" )
+    $("#history").append("<span class='div_class'>" + tmp1 + " + " + tmp2 + " = " + (tmp1 / tmp2) + "<span>" + "<br>")
 }
 
 
-function setup(){
+function setup() {
     console.log("setup() got called!");
     jQuery("#add").click(f1)
     jQuery("#sub").click(f2)
