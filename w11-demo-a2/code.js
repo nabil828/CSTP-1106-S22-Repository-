032e663e74b48cbab0d37c367f802be1bd9f465f
Nabil M. Al-Rousan <rousani@gmail.com>
2022-07-15 10:49:45 -0800

    done

--- w11-demo-a2/code.js
+++ w11-demo-a2/code.js
@@ -1,9 +1,14 @@
-function process_res(data){
+function process_res(data) {
     console.log(data);
+    for (i = 0; i < data.results.length; i++){
+        $("#result").append(JSON.stringify(data.results[i].original_title) + "<br>")
+        $("#result").append(JSON.stringify(data.results[i].overview) + "<br><br>")
+        $("#result").append(`<img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}">`)
+    }
     //todo
 }
 
-function search_(){
+function search_() {
     y = $("#y").val()
     $.ajax({
         url: `https://api.themoviedb.org/3/search/movie?api_key=ed4ef9b0f9bcb9c237ab83a2c2ffb909&language=en-US&query=${y}&page=1&include_adult=false`,
@@ -11,7 +16,7 @@ function search_(){
         success: process_res
     })
 }
-function setup () {
+function setup() {
     $("#x").click(search_)
 }
 
